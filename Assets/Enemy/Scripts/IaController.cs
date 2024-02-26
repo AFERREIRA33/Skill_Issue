22a698af23f81c9867809e23e0e86e8af5374e89
Echalaye <etienne.chalaye@gmail.com>
2024-02-26 16:59:14 +0100

    beginning path finding

--- Assets/Enemy/Scripts/IaController.cs
+++ Assets/Enemy/Scripts/IaController.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class Ia_Controller : MonoBehaviour
+public class IaController : MonoBehaviour
 {
+    private GameObject player;
+    private float distance = 1.3f;
+    public Tilemap obstacle;
+    private List<Vector2> nextMove = new List<Vector2>();
+    private bool findPath = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        findPath= true;
+        SearchPath();
+        this.transform.position = nextMove[0];
+    }
+
+    void SearchPath()
+    {
+        while (findPath)
+        {
+
+        }
     }
 }
