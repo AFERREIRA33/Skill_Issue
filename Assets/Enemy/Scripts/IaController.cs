98addb6de8a0ba8f9aa9911ef08fdce7bcd1eb09
Echalaye <etienne.chalaye@gmail.com>
2024-02-26 22:29:18 +0100

    begining path finding

--- Assets/Enemy/Scripts/IaController.cs
+++ Assets/Enemy/Scripts/IaController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -9,8 +10,18 @@ public class IaController : MonoBehaviour
     private GameObject player;
     private float distance = 1.3f;
     public Tilemap obstacle;
-    private List<Vector2> nextMove = new List<Vector2>();
-    private bool findPath = false;
+    private List<Vector3> nextMove = new List<Vector3>();
+    private bool findPath = true;
+    private Vector3 savePathCross;
+    private Dictionary<string, float> pathDico = new Dictionary<string, float>() { 
+        { "up", 0f },
+        { "down", 0f }, 
+        { "left", 0f },
+        { "right", 0f } };
+
+    private Tile testedTilePath;
+    private Vector3 posTested;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +31,9 @@ public class IaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        findPath= true;
+        //findPath= true;
         SearchPath();
-        this.transform.position = nextMove[0];
+        //this.transform.position = nextMove[0];
     }
 
     void SearchPath()
@@ -30,6 +41,45 @@ public class IaController : MonoBehaviour
         while (findPath)
         {
 
+            pathDico["up"] = Vector3.Distance(new Vector3(transform.position.x, transform.position.y + distance, 0), player.transform.position);
+            pathDico["down"] = Vector3.Distance(new Vector3(transform.position.x, transform.position.y - distance, 0), player.transform.position);
+            pathDico["left"] = Vector3.Distance(new Vector3(transform.position.x - distance, transform.position.y, 0), player.transform.position);
+            pathDico["right"] = Vector3.Distance(new Vector3(transform.position.x + distance, transform.position.y, 0), player.transform.position);
+            float min = pathDico.Values.Min();
+            string pathDicoKey = pathDico.First(entry => entry.Value == min).Key;
+
+            if (NoWall(pathDicoKey))
+            {
+
+            }
+
+            findPath= false;
         }
+    }
+
+    bool NoWall(string posTry)
+    {
+        if(posTry == "up")
+        {
+            posTested = new Vector3(transform.position.x, transform.position.y + distance, 0);
+        }else if(posTry == "down")
+        {
+            posTested = new Vector3(transform.position.x, transform.position.y - distance, 0);
+        }
+        else if (posTry == "right")
+        {
+            posTested = new Vector3(transform.position.x + distance, transform.position.y, 0);
+        }
+        else if (posTry == "left")
+        {
+            posTested = new Vector3(transform.position.x - distance, transform.position.y, 0);
+        }
+
+        testedTilePath = obstacle.GetTile<Tile>(new Vector3Int(Mathf.FloorToInt(posTested.x), Mathf.FloorToInt(posTested.y), Mathf.FloorToInt(posTested.z)));
+        if (testedTilePath != null)
+        {
+            return false;
+        }
+        return true;
     }
 }
